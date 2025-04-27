a6065bdc49e008ae956f6ac846cc03414e4d7416
Mikhail_Stankevich <Mikhail_Stankevich@epam.com>
2025-04-27 16:10:36 +0200

    chore: turn off speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+// import { SpeedInsights } from "@vercel/speed-insights/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <SpeedInsights />
+        {/*<SpeedInsights />*/}
         <Router />
       </ThemeProvider>
     </Provider>
