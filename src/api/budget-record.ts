b30ce8c992a1a89ee119860250a98fdb9a9a42b3
Mikhail_Stankevich <Mikhail_Stankevich@epam.com>
2025-04-27 16:06:01 +0200

    fix: api url

--- src/api/budget-record.ts
+++ src/api/budget-record.ts
@@ -1,8 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const budgetApi = createApi({
   reducerPath: "budgetApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "/api/budget-record" }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${API_URL}/api/budget-record` }),
   endpoints: (builder) => ({
     getBudgetRecords: builder.query<{ _id: string; category: string; value: number }[], void>({
       query: () => "/",
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import react from "@vitejs/plugin-react";
 import browserslistToEsbuild from "browserslist-to-esbuild";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  const PROXY = {
-    target: env.VITE_API_URL,
-    changeOrigin: true,
-  };
-
+export default defineConfig(() => {
   return {
     build: {
       outDir: "dist",
@@ -27,9 +20,6 @@ export default defineConfig(({ mode }) => {
     server: {
       open: true,
       port: 2022,
-      proxy: {
-        "/api": PROXY,
-      },
     },
   };
 });
